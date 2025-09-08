1be4cbc71d8d5723ae1ee7e6b771d279b313a3a4
LDY <lte6069@naver.com>
2025-09-08 18:54:29 +0900

    회원가입 / 서버 기능 구현

---
+++ game.js
@@ -0,0 +1,60 @@
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = function(server) {
+    const io = require('socket.io')(server, {
+        transport: ['websocket']
+    });
+
+    // 방 정보
+    var rooms = [];                 // 게임 대기방
+    var socketRooms = new Map();    // 게임 진행방
+
+    io.on('connection', (socket) => {
+        // 서버 구현
+        console.log('A USer connected:', socket.id);
+
+        // 특정 Socket(클라이언트)이 방에 입장했을 때 처리
+        // 1. 대기방에 방이 있음녀 입장
+        // 2. 대기방에 방이 없으면 새로 생성 후 입장
+        if(rooms.length > 0){
+            var roomId = rooms.shift();
+            socket.join(roomId);
+            socket.emit('joinRoom', { roomId: roomId });
+            socket.to(roomId).emit('StartGame', { roomId: roomId });
+            socketRooms.set(socket.id, roomId);
+        } else {
+            var roomId = uuidv4();
+            socket.join(roomId);
+            socket.emit('CreateRoom', { roomId: roomId });
+            rooms.push(roomId);
+            socketRooms.set(socket.id, roomId);
+        }
+
+        // 방을 떠날 때 처리
+        socket.on('leaveRoom', function(data) {
+            var roomId = data.roomId;
+            socket.leave(roomId);
+            socket.emit('exitRoom');
+            socket.to(roomId).emit('endGame');
+
+            // 혼자 들어간 방에서 나갈 때 방 삭제
+            const roomIdx = rooms.indexOf(roomId);
+            if(roomIdx !== -1) {
+                rooms.splice(roomIdx, 1);
+                console.log('Room deleted: ', roomId);
+            }
+
+            // 방 나간 소켓 정보 삭제
+            socketRooms.delete(socket.id);
+        });
+
+        // 소켓(클라이언트)가 특정 Block을 터치했을 때 처리
+        socket.on('doPlayer', function(playerInfo) {
+            var roomId = playerInfo.roomId;
+            var blockIndex = playerInfo.blockIndex;
+
+            console.log('Player action in room: ', roomId, " Block index : ", blockIndex);
+            socket.to(roomId).emit('doOpponent', {blockIndex: blockIndex});
+        });
+    });
+};
--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ var ResponseType = {
   INVALID_USERNAME: 0,
   INVALID_PASSWORD: 1,
   SUCCESS: 2,
+  Fail: 3,
 }
 
 /* GET users listing. */
@@ -24,7 +25,7 @@ router.post('/signup', async function(req, res, next) {
 
     // 입력값 검증
     if (!username || !password || !nickname) {
-      return res.status(400).json({ message: 'All fields are required.' });
+      return res.status(400).json({ result : ResponseType.Fail });
     }
 
     // DB 연결
@@ -34,7 +35,7 @@ router.post('/signup', async function(req, res, next) {
     // 중복된 username 확인
     var existingUser = await users.findOne({ username: username });
     if (existingUser) {
-      return res.status(409).json({ message: 'Username already exists.' });
+      return res.status(409).json({ result : ResponseType.INVALID_USERNAME });
     }
 
     // 비밀번호 암호화
@@ -49,7 +50,7 @@ router.post('/signup', async function(req, res, next) {
       createdAt: new Date()
     });
 
-    res.status(201).json({ message: 'User registered successfully.' });
+    res.status(201).json({ result : ResponseType.SUCCESS });
   } catch (error) {
     console.error('Error during signup:', error);
     res.status(500).json({ message: 'Internal server error.' });
@@ -97,4 +98,94 @@ router.post('/login', async function(req, res, next) {
   }
 });
 
+// 로그아웃
+router.get('/signout', function(req, res, next) {
+  if (req.session) {
+    // 세션 삭제
+    req.session.destroy(function(err) {
+      if (err) {
+        return res.status(500).json({ message: 'Failed to log out.' });
+      } else {
+        return res.json({ message: 'Logged out successfully.' });
+      }
+    });
+  } else {
+    res.json({ message: 'No active session.' });
+  }
+});
+
+// 마지막 점수 업데이트
+router.post('/addScore', async function(req, res, next){
+  try {
+    if(!req.session.isAuthenticated){
+      return res.status(403).json({ message: 'Unauthorized' });
+    }
+
+    var userid = req.session.userId;
+    var score = req.body.score;
+
+
+    if(!score || isNaN(score)){
+      return res.status(400).json({ message: 'Invalid score' });
+    }
+
+    // DB 연결
+    var database = req.app.get('database');
+    var users = database.collection('users');
+
+    const result = await users.updateOne(
+      { _id : new ObjectId(userid) },
+      {
+        $set: {
+          score : Number(score),
+          updateAt: new Date()
+        }
+      }
+    );
+
+    if(result.matchedCount === 0){
+      return res.status(404).json({ message : 'User not found' });
+    }
+
+    res.status(200).json({ message: 'Score updated successfully' });
+  }
+  catch(err) {
+    console.error('Error updating score:', err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// 점수 조회
+router.get('/score', async function(req, res, next) {
+  try {
+    if(!req.session.isAuthenticated){
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    var userid = req.session.userId;
+
+    // DB 연결
+    var database = req.app.get('database');
+    var users = database.collection('users');
+
+    const user = await users.findOne(
+      { _id : new ObjectId(userid) }
+    );
+
+    if(!user){
+      return res.status(404).json({ message: 'User Not found' });
+    }
+
+    res.json({
+      id: user._id.toString(),
+      username: user.username,
+      nickname: user.nickname,
+      score: user.score || 0
+    });
+
+  } catch(err){
+    console.error('Error updating score:', err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+})
 module.exports = router;
